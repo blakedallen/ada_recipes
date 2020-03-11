acdc592eaccf865188c28f463efed48bb96f98ab
blakedallen <blakedallen@gmail.com>
2020-03-11 14:24:14 -0700

    adding in scatterplot recipe

---
+++ recipes/scatter.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import matplotlib
+
+def main(d, path=None, show=True):
+    """ create a scatterplot 
+        path: optional file location to save data
+        show: boolean, whether to render plot to screen
+    """
+    
+    p = plt.scatter(d["x"],d["y"])
+    plt.xticks(d["x"], d["x_titles"])
+    plt.xlabel(d["x_label"])
+    plt.ylabel(d["y_label"])
+    plt.title(d["title"])
+    if path:
+        plt.savefig(path)
+    if show:
+        plt.show()
+
+def define_data():
+    """ function to create our mock dataset """
+    d = {
+        "x":[0.0,1.0,2.0,3.0], #x coordinates of the points
+        "y":[1.2, 2.3, 1.7, 0.5], #y coordinates of the points
+        "x_label":"Provisions",
+        "y_label":"Amount",
+        "title":"Breakfast",
+    }
+    return d
+
+if __name__ == "__main__":
+    dataset = define_data()
+    main(dataset)
+
+
+
